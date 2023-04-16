62c20b9934c36fcd06de39420b13409279796523
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-04-16 14:35:14 +0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,51 +1,78 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import tensorflow as tf
-import cv2
-from PIL import Image
+import os
+import numpy as np
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from keras.models import load_model
+
+# Authenticate and create PyDrive client
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+# Set the ID of the saved model file on Google Drive
+file_id = '1eujUxgPtbBHQKi9qBqpxKR3UyPelsvTv'
+
+# Download the model file from Google Drive
+downloaded = drive.CreateFile({'id': file_id})
+downloaded.GetContentFile('model_vgg19.h5')
+
+# Load the saved Keras model
+model = load_model('model_vgg19.h5')
+
+# Use the model to make predictions or other tasks
+# ...
 
 
-# Load the trained model
-url = 'https://drive.google.com/file/d/1eujUxgPtbBHQKi9qBqpxKR3UyPelsvTv/view?usp=share_link'
-model_load = tf.keras.models.load_model(url)
-
-# Define the class labels
-labels = ['label1', 'label2', 'label3', 'label4', 'label5', 'label6']
-
-# Get the uploaded image file
-img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 
-if img_file_buffer is not None:
-    # Open the image and convert it to a numpy array
-    image = Image.open(img_file_buffer)
-    img_array = np.array(image)
+# import numpy as np
+# import pandas as pd
+# import streamlit as st
+# import tensorflow as tf
+# import cv2
+# from PIL import Image
 
-# Normalization
-normalization_layer = tf.keras.layers.Rescaling(1./255)
 
-# If the "Predict" button is clicked
-if st.button('Predict'):
-    # view the image
-    st.image(img_array)
-    try:
-        # Resize the image to match the input size of the model
-        img_array = normalization_layer(cv2.resize(img_array.astype('uint8'), (224, 224)))
+# # Load the trained model
+# model_load = tf.keras.models.load_model(url)
 
-        # Add an extra dimension to represent the batch size of 1
-        img_array = np.expand_dims(img_array, axis=0)
+# # Define the class labels
+# labels = ['label1', 'label2', 'label3', 'label4', 'label5', 'label6']
 
-        # Get the predicted probabilities for each class
-        val = model_load.predict(img_array)
+# # Get the uploaded image file
+# img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-        # Get the index of the class with the highest probability
-        predicted_index = np.argmax(val[0])
 
-        # Get the label corresponding to the predicted class
-        predicted_label = labels[predicted_index]
+# if img_file_buffer is not None:
+#     # Open the image and convert it to a numpy array
+#     image = Image.open(img_file_buffer)
+#     img_array = np.array(image)
 
-        font_size = "24px"
-        st.markdown("<h4 style='text-align: left; color: #2F3130; font-size: {};'>{}</h4>".format(font_size, predicted_label), unsafe_allow_html=True)
-    except:
-        pass
+# # Normalization
+# normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+# # If the "Predict" button is clicked
+# if st.button('Predict'):
+#     # view the image
+#     st.image(img_array)
+#     try:
+#         # Resize the image to match the input size of the model
+#         img_array = normalization_layer(cv2.resize(img_array.astype('uint8'), (224, 224)))
+
+#         # Add an extra dimension to represent the batch size of 1
+#         img_array = np.expand_dims(img_array, axis=0)
+
+#         # Get the predicted probabilities for each class
+#         val = model_load.predict(img_array)
+
+#         # Get the index of the class with the highest probability
+#         predicted_index = np.argmax(val[0])
+
+#         # Get the label corresponding to the predicted class
+#         predicted_label = labels[predicted_index]
+
+#         font_size = "24px"
+#         st.markdown("<h4 style='text-align: left; color: #2F3130; font-size: {};'>{}</h4>".format(font_size, predicted_label), unsafe_allow_html=True)
+#     except:
+#         pass
